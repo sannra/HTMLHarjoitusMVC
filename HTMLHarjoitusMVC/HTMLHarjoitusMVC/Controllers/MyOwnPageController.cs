a06c3fd95132d077aa5e3aa8b2cce360dd661e5e
Sanna-Leena Räsänen <sanna-leena.rasanen@student.careeria.fi>
2019-05-22 09:49:31 +0300

    Tallennukset 22.5.2019.

---
+++ HTMLHarjoitusMVC/HTMLHarjoitusMVC/Controllers/MyOwnPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHarjoitusMVC.Controllers
+{
+    public class MyOwnPageController : Controller
+    {
+        // GET: MyOwnPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
